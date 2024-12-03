640355228b45261b1a295e8f79b9d28be987bade
Ridho <89362181+RidhoNGPX@users.noreply.github.com>
2024-12-03 15:47:20 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'cc4fe4b2-4a87-448a-bd07-4f1835dac02a';
 
-const พร็อกซีไอพีs = ['129.150.50.63'];
+const พร็อกซีไอพีs = ['172.232.239.151'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
